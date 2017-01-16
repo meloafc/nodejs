0fcb8ebe59089ddaaac95211e33c43a0cc18d366
Alex Fabiano <afabiano15@gmail.com>
2017-01-16 17:27:23 -0400

    isolando o select

--- casadocodigo/app/routes/produtos.js
+++ casadocodigo/app/routes/produtos.js
@@ -1,9 +1,10 @@
 module.exports = function(app){
     app.get('/produtos',function(req,res){
         var connection = app.infra.connectionFactory();
+        var produtosBanco = app.infra.produtosBanco;
 
-        connection.query('select * from livros', function(err,results){
-            res.render('produtos/lista', {lista:results} );
+        produtosBanco.lista(connection, function(erros, resultados) {
+            res.render('produtos/lista', {lista:resultados} );
         });
 
         connection.end();
