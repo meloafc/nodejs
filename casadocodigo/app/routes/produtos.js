f3a62334901a43219a62b21478241a61f555ddc8
Alex <afabiano15@gmail.com>
2017-01-17 08:59:34 -0400

    criacao do metodo salvar produto

--- casadocodigo/app/routes/produtos.js
+++ casadocodigo/app/routes/produtos.js
@@ -1,9 +1,9 @@
 module.exports = function(app) {
     app.get('/produtos',function(req,res) {
         var connection = app.infra.connectionFactory();
-        var produtosBanco = new app.infra.ProdutosDAO(connection);
+        var produtosDAO = new app.infra.ProdutosDAO(connection);
 
-        produtosBanco.lista(function(erros, resultados) {
+        produtosDAO.lista(function(erros, resultados) {
             res.render('produtos/lista', {lista:resultados} );
         });
 
@@ -12,5 +12,15 @@ module.exports = function(app) {
 
     app.get('/produtos/form',function(req,res) {
 	    res.render('produtos/form');
+	});
+
+	app.post('/produtos/salva',function(req,res) {
+	    var produto = req.body;
+	    var connection = app.infra.connectionFactory();
+	    var produtosDAO = new app.infra.ProdutosDAO(connection);
+
+	    produtosDAO.salva(produto, function(erros,resultados) {
+	        res.render('produtos/lista');
+	    });
 	});
 }
