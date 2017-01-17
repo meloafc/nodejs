95cd754715db3b602fe2ef93c5304e6601858e40
Alex Fabiano <afabiano15@gmail.com>
2017-01-17 16:58:44 -0400

    melhorando as rotas

--- casadocodigo/app/routes/produtos.js
+++ casadocodigo/app/routes/produtos.js
@@ -1,21 +1,19 @@
 module.exports = function(app) {
 
-	var listaProdutos = function(req,res) {
+    app.get('/produtos', function(req,res) {
 	    var connection = app.infra.connectionFactory();
 	    var produtosDAO = new app.infra.ProdutosDAO(connection);
 	    produtosDAO.lista(function(erros,resultados) {
 	        res.render('produtos/lista', {lista:resultados} );
 	    });
 	    connection.end();
-	};
-
-    app.get('/produtos', listaProdutos);
+	});
 
     app.get('/produtos/form', function(req,res) {
 	    res.render('produtos/form');
 	});
 
-	app.post('/produtos/salva',function(req,res) {
+	app.post('/produtos',function(req,res) {
 	    var produto = req.body;
 	    var connection = app.infra.connectionFactory();
 	    var produtosDAO = new app.infra.ProdutosDAO(connection);
