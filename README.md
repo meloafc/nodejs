4df01f017ee3f6195c2a5137af7b0b16c73b2124
Alex Fabiano <afabiano15@gmail.com>
2017-01-17 18:14:05 -0400

    criando validacao de produto

--- README.md
+++ README.md
@@ -3,15 +3,9 @@ Curso Node.js: Inovando com Javascript no backend
 
 # Inicio
 npm init -> iniciar o package.json
+
 npm install -> instala as dependecias do package.json
 
 node app.js -> executa o servidor
 
-# Modulos
-	- express
-	- ejs
-	- mysql
-	- express load
-	- body-parser
-
-	- nodemon -> reinicia o servidor automaticamente. (nodemon app -> executa o servidor pelo nodemon)
+- nodemon -> reinicia o servidor automaticamente. (nodemon app -> executa o servidor pelo nodemon)
--- casadocodigo/app/routes/produtos.js
+++ casadocodigo/app/routes/produtos.js
@@ -11,19 +11,35 @@ module.exports = function(app) {
 				json: function() {
 					res.json(resultados);
 				}
-			});	        
+			});
 	    });
 	    connection.end();
 	});
 
     app.get('/produtos/form', function(req,res) {
-	    res.render('produtos/form');
+	    res.render('produtos/form', {errosValidacao: {}, produto: {}});
 	});
 
 	app.post('/produtos',function(req,res) {
 	    var produto = req.body;
 	    var connection = app.infra.connectionFactory();
 	    var produtosDAO = new app.infra.ProdutosDAO(connection);
+
+		req.assert('titulo', 'Título é obrigratório').notEmpty();
+		req.assert('preco', 'Formato inválido').isFloat();
+
+		var erros = req.validationErrors();
+		if(erros) {
+			res.format({
+				html: function() {
+					res.status(400).render('produtos/form', {errosValidacao : erros, produto: produto});
+				},
+				json: function() {
+					res.status(400).json(erros);
+				}
+			});			
+			return;
+		}
 
 	    produtosDAO.salva(produto, function(erros,resultados) {
 	        res.redirect('/produtos');
