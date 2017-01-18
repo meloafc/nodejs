626d3b0e2d0941867fe6fbeacc85f8fa6a6d4ba7
Alex Fabiano <afabiano15@gmail.com>
2017-01-18 19:15:02 -0400

    testando produtos response

--- casadocodigo/test/produtos.js
+++ casadocodigo/test/produtos.js
@@ -1,5 +1,19 @@
-describe('#ProdutosController', function(){
-    it('#listagem json',function(){
-        console.log("teste de verificacao de listagem json");
+var http = require('http');
+var assert = require('assert');
+describe('ProdutosController', function () {
+    it('listagem json', function (done) {
+        var configuracoes = {
+            hostname: 'localhost',
+            port: 3000,
+            path: '/produtos',
+            headers: {
+                'Accept': 'application/json'
+            }
+        };
+        http.get(configuracoes, function (res) {
+            assert.equal(res.statusCode, 200);
+            assert.equal(res.headers['content-type'], 'application/json; charset=utf-8');
+            done();
+        });
     });
 });
